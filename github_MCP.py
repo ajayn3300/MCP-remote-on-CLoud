1284468ab708851ab32d20978c0eccc8860505e2
Ajay Negi <96326989+ajayn3300@users.noreply.github.com>
2026-09-12 14:39:20 +0530

    mcp server with remote acesss

---
+++ github_MCP.py
@@ -0,0 +1,38 @@
+import os
+from github import Github, Auth
+from dotenv import load_dotenv
+load_dotenv()
+from mcp.server.fastmcp import FastMCP
+
+#intialize server
+mcp = FastMCP('GITHUB')
+
+@mcp.tool()
+def get_git_repo(repo_link :str) ->dict:
+
+    '''this function takes github repository link as an argument and return all the python releated content with their file names as a dictionery'''
+    link = repo_link.replace("https://github.com/",'')
+
+    # Retrieve the token
+    token = os.getenv("GITHUB_API_KEY")
+
+    #gitub
+    git = Github(auth = Auth.Token(token))
+
+    #getting repo
+    repo = git.get_repo(link)
+
+    # extracting only code content
+    contents = [i for i in repo.get_contents('') if i.name.endswith(('py','ipynb'))]
+
+    data = {file.name:file.decoded_content.decode("utf-8") for file in contents}
+    return data
+
+
+
+if __name__ == "__main__":
+    # Runs the server using standard input/output (stdio)
+    # mcp.run(transport="stdio")  #for local
+    mcp.run(transport="http", host="0.0.0.0", port=8000) # remote
+
+
